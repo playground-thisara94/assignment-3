39302557666a252a32b2080ed8d010a63bacfda6
Thisara Rathnapala <thisaratr94@gmail.com>
2023-08-08 02:10:49 +0530

    Final Commit

--- Assignment3.java
+++ Assignment3.java
@@ -9,37 +9,40 @@ public class Assignment3 {
         entireCode:
         {
 
-            // System.out.print("Enter your name: ");
-            // String name = scanner.nextLine();
+            String red = "\033[31m";
+            String white = "\033[30m";
 
-            // if (name.startsWith(" ") | name.length()==0) {
-            //     System.out.printf("%sInvalid Name%s \n","\033[31m","\033[30m");
-            //     break entireCode;
-            // }
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
 
-            // System.out.print("Enter your age: ");
-            // int age = scanner.nextInt();
+            if (name.startsWith(" ") | name.length()==0) {
+                System.out.printf("%sInvalid Name%s \n",red,white);
+                break entireCode;
+            }
 
-            // if (age < 10 | age > 18) {
-            //     System.out.printf("%sInvalid Age%s \n","\033[31m","\033[30m");
-            //     break entireCode;
-            // }
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
 
-            //scanner.nextLine();
+            if (age < 10 | age > 18) {
+                System.out.printf("%sInvalid Age%s \n",red,white);
+                break entireCode;
+            }
+
+            scanner.nextLine();
 
             System.out.print("Enter your Subject 1: ");
             String subject1 = scanner.nextLine();
 
             if (!subject1.startsWith("SE-")) {
-                System.out.printf("%sInvalid Subject%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Subject%s \n",red,white);
                 break entireCode;
             }
 
             System.out.print("Enter your Marks of "+subject1+": ");
-            int marks1 = scanner.nextInt();
+            double marks1 = scanner.nextDouble();
 
             if (marks1 < 0 | marks1 > 100) {
-                System.out.printf("%sInvalid Marks%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Marks%s \n",red,white);
                 break entireCode;
             }
 
@@ -49,15 +52,15 @@ public class Assignment3 {
             String subject2 = scanner.nextLine();
 
             if (!subject2.startsWith("SE-") | subject2.equals(subject1)) {
-                System.out.printf("%sInvalid Subject%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Subject%s \n",red,white);
                 break entireCode;
             }
 
             System.out.print("Enter your Marks of "+subject2+": ");
-            int marks2 = scanner.nextInt();
+            double marks2 = scanner.nextDouble();
 
             if (marks2 < 0 | marks2 > 100) {
-                System.out.printf("%sInvalid Marks%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Marks%s \n",red,white);
                 break entireCode;
             }
 
@@ -67,18 +70,61 @@ public class Assignment3 {
             String subject3 = scanner.nextLine();
 
             if (!subject3.startsWith("SE-") | subject3.equals(subject1) | subject3.equals(subject2)) {
-                System.out.printf("%sInvalid Subject%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Subject%s \n",red,white);
                 break entireCode;
             }
 
-            System.out.print("Enter your Marks of "+subject3+": ");
-            int marks3 = scanner.nextInt();
+            System.out.print("Enter your Marks of "+subject3+": "); 
+            double marks3 = scanner.nextDouble();
 
             if (marks3 < 0 | marks3 > 100) {
-                System.out.printf("%sInvalid Marks%s \n","\033[31m","\033[30m");
+                System.out.printf("%sInvalid Marks%s \n",red,white);
                 break entireCode;
             }
 
+            System.out.println();
+            System.out.println("---------------------------------------");
+            System.out.println();
+
+            // String blueBold = "\033[34;1m";
+            // String blue = "\033[34m";
+            // String greenBold = "\033[32;1m";
+            // String green = "\033[32m";
+            // String yellowBold = "\033[33;1m";
+            // String yellow = "\033[33m";
+            // String redBold = "\033[31;1m";
+            // String whiteNotBold = "\033[30;0m";
+
+            double total = marks1 + marks2 + marks3;
+            double average = total/3;
+            String status = average>=75 ? "\033[34;1mDP\033[30;0m" : 
+                            average>=65 ? "\033[32;1mCP\033[30;0m" : 
+                            average>=55 ? "\033[33;1mP\033[30;0m" : "\033[31;1mF\033[30;0m";
+            
+            String status1 = marks1>=75 ? "\033[34mDP\033[30m" : 
+                            marks1>=65 ? "\033[32mCP\033[30m" : 
+                            marks1>=55 ? "\033[33mP\033[30m" : "\033[31mF\033[30m";
+
+            String status2 = marks2>=75 ? "\033[34mDP\033[30m" : 
+                            marks2>=65 ? "\033[32mCP\033[30m" : 
+                            marks2>=55 ? "\033[33mP\033[30m" : "\033[31mF\033[30m";
+
+            String status3 = marks3>=75 ? "\033[34mDP\033[30m" : 
+                            marks3>=65 ? "\033[32mCP\033[30m" : 
+                            marks3>=55 ? "\033[33mP\033[30m" : "\033[31mF\033[30m";
+
+            System.out.printf("Name : %s%s%s \n","\033[34;1m",name.toUpperCase(),"\033[30;0m");
+            System.out.printf("Age : %s years old \n",age);
+            System.out.printf("Status : %s \n",status);
+            System.out.printf("Total : %.2f     Avg : %.2f \n", total,average);
+            System.out.println();
+            System.out.printf("+%s+%2$s+%2$s+ \n", "-".repeat(10),"-".repeat(8));
+            System.out.printf("|%-10s|%8s|%8s| \n", "Subject", "Marks", "Status");
+            System.out.printf("+%s+%2$s+%2$s+ \n", "-".repeat(10),"-".repeat(8));
+            System.out.printf("|SE-%03d%s|% 8.2f|%18s| \n", Integer.valueOf(subject1.substring(3))," ".repeat(4),marks1,status1);
+            System.out.printf("|SE-%03d%s|% 8.2f|%18s| \n", Integer.valueOf(subject2.substring(3))," ".repeat(4),marks2,status2);
+            System.out.printf("|SE-%03d%s|% 8.2f|%18s| \n", Integer.valueOf(subject3.substring(3))," ".repeat(4),marks3,status3);
+            System.out.printf("+%s+%2$s+%2$s+ \n", "-".repeat(10),"-".repeat(8));
         }
 
     }
